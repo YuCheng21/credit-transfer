ff7d44f31c843b13d87639740c0ff8267e2dedf7
YuCheng <fyc621@gmail.com>
2021-08-05 21:25:25 +0800

    adjust: replace Dockerfile to docker-compose.yml. #20210805002

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.7
-ADD . /code
-WORKDIR /code
-RUN pip install -r requirements.txt
-EXPOSE 8080
-CMD ['python', 'main.py']
---
+++ docker/mariadb/data.sql
@@ -0,0 +1,4 @@
+USE `credit-transfer`;
+
+INSERT INTO `credit-transfer`.admin (id, username, password, email, real_name)
+VALUES (1, 'admin', 'admin', 'nkust.ee.ct@gmail.com', '管理員');
